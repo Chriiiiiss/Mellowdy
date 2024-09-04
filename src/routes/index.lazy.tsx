98ae005197c083010ce5de27ddd0befc90d18274
Chris <chris.fraanc@gmail.com>
2024-09-04 11:13:24 +0200

    [Login] : Add user context to the router, add Dashboard.tsx to try the router guard

--- src/routes/index.lazy.tsx
+++
@@ -1,7 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router';
-
-import { LoginPage } from '../pages/Login';
-
-export const Route = createLazyFileRoute('/')({
-  component: LoginPage,
-});

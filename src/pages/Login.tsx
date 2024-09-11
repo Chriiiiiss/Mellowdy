7027c237fc9ac2f43196b661ce167cd73035107f
Loris Dié <loris.die@converteo.com>
2024-09-11 14:25:41 +0200

    [ONBOARDING] redesign connection page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,60 @@
-import { Button, Flex, Heading } from '@radix-ui/themes';
-import { LoginLayout } from '../layout/LoginLayout';
-import { PersonIcon } from '@radix-ui/react-icons';
+import { Box, Container, Grid, Text } from '@radix-ui/themes';
 import styled from 'styled-components';
+import { OnboardingLayout } from '../layout/OnboardingLayout';
+import { MainTitle } from '../components/MainTitle';
+import { OnboardingCard } from '../components/onboarding/Card';
+import { OnboardingButton } from '../components/onboarding/Button';
 
-const SSOButton = styled(Button).attrs({ size: '4' })``;
+const CustomBox = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CustomText = styled(Text)`
+  font-family: var(--default-font-family);
+  font-size: 16px;
+  margin: 0;
+  font-weight: 600;
+  color: var(--mellowdy-black);
+  text-align: center;
+`;
+
+const ProviderList = [
+  {
+    label: 'Spotify',
+    icon: 'mdi:spotify',
+  },
+  {
+    label: 'Apple Music',
+    icon: 'simple-icons:applemusic',
+  },
+];
 
 export const LoginPage = () => {
   return (
-    <LoginLayout>
-      <Heading align={'center'} as={'h1'}>
-        Mellowdy
-      </Heading>
-      <Flex gap="3" direction={'column'} align={'center'} justify={'center'}>
-        <SSOButton>
-          <PersonIcon /> Sign in with Spotify
-        </SSOButton>
-        <SSOButton>
-          <PersonIcon /> Sign in with Deezer
-        </SSOButton>
-      </Flex>
-      <Button>Submit</Button>
-    </LoginLayout>
+    <OnboardingLayout>
+      <Box>
+        <OnboardingCard>
+          <Container align={'center'} minHeight={'530px'}>
+            <Grid columns={'1'} gap={'8'} justify={'center'}>
+              <CustomBox>
+                <MainTitle variant="secondary" />
+              </CustomBox>
+              <CustomText>Connectez votre appli de musique préférée</CustomText>
+              {ProviderList.map((provider, index) => (
+                <CustomBox key={index}>
+                  <OnboardingButton
+                    onClick={() => console.log(provider.label)}
+                    label={provider.label}
+                    iconStart={provider.icon}
+                  />
+                </CustomBox>
+              ))}
+            </Grid>
+          </Container>
+        </OnboardingCard>
+      </Box>
+    </OnboardingLayout>
   );
 };

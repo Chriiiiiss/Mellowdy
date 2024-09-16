ca0e1dd2c4d412632c652675f6813f9ff1ab9ba9
Loris Dié <loris.die@converteo.com>
2024-09-16 15:49:00 +0200

    [ONBOARDING] add form validation + redirection

--- src/components/register/Form.tsx
+++ src/components/register/Form.tsx
@@ -3,18 +3,27 @@ import { Avatar, Flex, Grid } from '@radix-ui/themes';
 import { useState } from 'react';
 import { FormField } from './Field';
 import { OnboardingButton } from '../onboarding/Button';
+import { useNavigate } from '@tanstack/react-router';
 
 export const RegisterForm = () => {
   const [imageUrl, setImageUrl] = useState('');
+  const [username, setUsername] = useState('');
+  const navigate = useNavigate();
 
   return (
-    <Form.Root style={{ width: '200px' }}>
+    <Form.Root
+      style={{ width: '200px' }}
+      onSubmit={() => {
+        navigate({ to: '/homePage' });
+      }}
+    >
       <FormField
         label="Username"
         type="text"
         valueMissing="Mising username"
         typeMismatch="Please provide a valid username"
         required
+        onChange={(e) => setUsername(e.target.value)}
       />
 
       <FormField
@@ -27,7 +36,12 @@ export const RegisterForm = () => {
 
       <Grid gap={'6'}>
         <Flex justify={'center'}>
-          <Avatar src={imageUrl} size={'8'} alt="User Avatar" fallback="U" />
+          <Avatar
+            src={imageUrl}
+            size={'8'}
+            alt="User Avatar"
+            fallback={username.slice(0, 2).toUpperCase()}
+          />
         </Flex>
 
         <Form.Submit asChild>

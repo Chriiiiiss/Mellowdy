af745f995ecfe3ce92d18cf03c16c93e46be2503
Chris <chris.fraanc@gmail.com>
2024-10-02 23:39:45 +0200

    [Auth] : Add persist to zustand and hydration middleware to check token

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,15 +1,7 @@
 export interface IUser {
   username?: string | null;
-  token?: string | null;
-  email?: string | null;
-  providerId?: number | null;
-  isAuth?: boolean | null;
 }
 
 export const userMock: IUser = {
   username: null,
-  token: null,
-  email: null,
-  providerId: null,
-  isAuth: false,
 };

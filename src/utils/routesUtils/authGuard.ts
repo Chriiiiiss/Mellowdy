afa15d73b4b78b6877a00a4fb59c82238a7e6241
Chris <chris.fraanc@gmail.com>
2024-10-03 00:08:38 +0200

    [FIX] : Fix dumb issues

--- src/utils/routesUtils/authGuard.ts
+++ src/utils/routesUtils/authGuard.ts
@@ -2,7 +2,7 @@ import { redirect } from '@tanstack/react-router';
 import { UserState } from '../../stores/useUserState';
 
 export const checkAuth = (userState: UserState | undefined) => {
-  if (userState && userState.isAuth) {
+  if (userState && !userState.isAuth) {
     throw redirect({ to: '/login' });
   }
 };

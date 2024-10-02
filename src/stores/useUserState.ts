db89f05d816c0a7c5eebfea9a649b639f7e503f2
Chris <chris.fraanc@gmail.com>
2024-10-02 23:39:45 +0200

    [Auth] : Add persist to zustand and hydration middleware to check token

--- src/stores/useUserState.ts
+++ src/stores/useUserState.ts
@@ -1,19 +1,41 @@
 import { create } from 'zustand';
 import { IUser } from '../interfaces/user';
+import { persist } from 'zustand/middleware';
+import { isTokenExpired } from '../utils/routesUtils/checkTokens';
 
 export type UserState = {
   user: IUser | null;
-  provider: string | null;
+  providerId: number | null;
+  token: string | null;
   isAuth: boolean;
-  login: (userData: IUser) => void;
+  login: (userData: IUser, token: string, providerId: number) => void;
   logout: () => void;
 };
 
 // User state
-export const useUserState = create<UserState>()((set) => ({
-  user: null,
-  provider: null,
-  isAuth: false,
-  login: (userData: IUser) => set({ user: userData, isAuth: true }),
-  logout: () => set({ user: null, isAuth: false }),
-}));
+export const useUserState = create<UserState>()(
+  persist(
+    (set) => ({
+      user: null,
+      providerId: null,
+      token: null,
+      isAuth: false,
+      login: (userData: IUser, token: string, providerId: number) =>
+        set({ user: userData, isAuth: true, token, providerId }),
+      logout: () => {
+        set({ user: null, isAuth: false, token: null, providerId: null });
+        useUserState.persist.clearStorage(); // Clear the localStorage on logout
+      },
+    }),
+    {
+      name: 'user-state',
+      onRehydrateStorage: (state) => {
+        // On hydratation of the state, check if the token is expired,
+        // if yes, yeet the user out
+        if (state.token && isTokenExpired(state.token)) {
+          state.logout();
+        }
+      },
+    }
+  )
+);

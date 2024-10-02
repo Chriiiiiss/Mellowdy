df22e5e5f63d934118b96717b9fcd0b3cc97be71
Chris <chris.fraanc@gmail.com>
2024-10-03 00:09:36 +0200

    [Chore] : clear console.log

--- src/routes/homePage/index.tsx
+++ src/routes/homePage/index.tsx
@@ -5,7 +5,6 @@ import { checkAuth } from '../../utils/routesUtils/authGuard';
 
 export const Route = createFileRoute('/homePage/')({
   beforeLoad: async ({ context }) => {
-    console.log(context.userState);
     checkAuth(context.userState);
   },
   component: HomePage,

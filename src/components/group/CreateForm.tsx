514a20954da2cfdd36ac8b861bafec8c7a4ca50e
Loris Die <loris.die@converteo.com>
2024-10-21 15:55:22 +0200

    [PAGES] : Add Create Group page + textArea component

---
+++ src/components/group/CreateForm.tsx
@@ -0,0 +1,53 @@
+import * as Form from '@radix-ui/react-form';
+import { useNavigate } from '@tanstack/react-router';
+import { Avatar, Flex } from '@radix-ui/themes';
+import { useState } from 'react';
+import { FormField } from '../Field';
+import { TextArea } from '../TextArea';
+import { MellowdyButton } from '../Button';
+
+export const GroupCreateForm = () => {
+  const [imageUrl, setImageUrl] = useState('');
+  const [groupName, setGroupName] = useState('');
+  const navigate = useNavigate();
+  return (
+    <Form.Root
+      style={{ width: '250px' }}
+      onSubmit={() => {
+        navigate({ to: '/groupDetails' });
+      }}
+    >
+      <Flex justify={'center'} style={{ marginBottom: '15px' }}>
+        <Avatar
+          src={imageUrl}
+          size={'8'}
+          alt="User Avatar"
+          fallback={groupName.slice(0, 5).toUpperCase()}
+          style={{ borderRadius: '50%' }}
+        />
+      </Flex>
+      <FormField
+        label={'Nom du groupe'}
+        type="text"
+        onChange={(e) => setGroupName(e.target.value)}
+        valueMissing="Missing group name"
+        required
+      />
+      <FormField
+        label={'URL icône du groupe'}
+        type="url"
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      <TextArea
+        label={'Description du groupe'}
+        valueMissing="Missing group desc"
+        maxLength={600}
+      />
+      <Flex justify={'center'}>
+        <Form.Submit asChild>
+          <MellowdyButton label="Créer le groupe" onClick={() => {}} />
+        </Form.Submit>
+      </Flex>
+    </Form.Root>
+  );
+};

a5bbe435164b3f9e108dacfbb7e1adb339a5ea15
elise.vue <evue@data-driven.fr>
2024-10-22 03:13:20 +0200

    [Profile] Fix css Navbar

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import process from 'process';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,7 +11,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: '/',
-    plugins: [react(), TanStackRouterVite()],
+    plugins: [react(), TanStackRouterVite(), svgr()],
     preview: {
       port: 3001,
       strictPort: true,

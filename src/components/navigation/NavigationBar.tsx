c9739c51a14c53caffad7a1fb1e52579879583f9
elise.vue <evue@data-driven.fr>
2024-10-22 17:26:18 +0200

    [Profile] Small width fixes

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -39,7 +39,7 @@ export const NavigationBar = () => {
   const location = useLocation();
   return (
     <Container
-      position={'sticky'}
+      position={'fixed'}
       bottom={'0'}
       left={'0'}
       size="4"

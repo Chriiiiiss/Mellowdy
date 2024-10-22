b6b0eb0a3a821e4055d724e766b24baacd18b7b0
Chris <chris.fraanc@gmail.com>
2024-10-23 00:43:54 +0200

    [FIX] : Fix the cors workaround

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import process from 'process';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react(), TanStackRouterVite()],
-  preview: {
-    port: 3001,
-    strictPort: true,
-  },
-  server: {
-    port: 3001,
-    strictPort: true,
-    host: true,
-    origin: 'http://localhost:3001',
-    proxy: {
-      '/v1': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/v1/, '/v1'),
+export default defineConfig(({ mode }) => {
+  // Workearound for CORS issue, get this out for production
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: '/',
+    plugins: [react(), TanStackRouterVite()],
+    preview: {
+      port: 3001,
+      strictPort: true,
+    },
+    server: {
+      port: 3001,
+      strictPort: true,
+      host: true,
+      origin: 'http://localhost:3001',
+      proxy: {
+        '/v1': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/v1/, '/v1'),
+        },
       },
     },
-  },
+  };
 });

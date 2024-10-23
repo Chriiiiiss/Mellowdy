b993459b7bfd2d1f6d2c851e5a88895e829b9361
Chris <chris.fraanc@gmail.com>
2024-10-23 12:54:18 +0200

    [FIX] : Remove old page, update routeTree.gen.ts

--- src/routes/groupCreate/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { GroupCreate } from '../../pages/GroupCreate';
-
-export const Route = createFileRoute('/groupCreate/')({
-  component: GroupCreate,
-});
